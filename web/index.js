08a3eebb072fcfb7c06e83e8c1ae7e57dcbcff69
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-02-18 11:06:56 +0530

    Add endpoint for Python script
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- web/index.js
+++ web/index.js
@@ -1,10 +1,19 @@
 const express = require("express"),
 	app = express(),
 	path = __dirname + "/views/",
-	morgan = require("morgan");
+	bodyParser = require("body-parser"),
+	morgan = require("morgan"),
+	{spawn} = require("child_process");
+
 app.use(morgan("dev"));
-app.set("port", 5000);
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
 app.use(express.static(__dirname + "/public"));
+app.set("port", 8000);
 app.set("views", path);
 app.set("view engine", "ejs");
 app.listen(app.get("port"), () => {
@@ -19,6 +28,19 @@ app.listen(app.get("port"), () => {
 
 app.get("/", (_req, res) => {
 	res.render("index");
+});
+app.post("/getRating", (req, res) => {
+	var pythonOutput;
+	console.log(req.body.input);
+	const python = spawn("python", ["get_rating.py", req.body.input]);
+	python.stdout.on("data", (data) => {
+		pythonOutput = data.toString();
+	});
+	python.on("close", (code) => {
+		console.log(`Python exit code: ${code}`);
+		console.log(pythonOutput);
+		res.json(pythonOutput);
+	});
 });
 
 app.use((_req, res) => {
