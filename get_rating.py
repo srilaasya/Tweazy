27975e629a7efeed34ccd9032f1eaf8dd67a89e0
Sri Laasya Nutheti <nutheti.laasya@gmail.com>
2021-02-25 00:55:39 +0530

    Add features to final py file

--- get_rating.py
+++ get_rating.py
@@ -8,15 +8,19 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from dotenv import load_dotenv
+load_dotenv()
 
-consumer_key = "UoZMEibxQYz2SAZm4vRq0Z9xC"
-consumer_secret = "7Q7t9iFPBEoo80D1U6QhOj2ZsQYTWDDW4wfhpWIyJGooCCxGQc"
-access_token = "1303429505256574976-BpAsIYb8Fk3Eb6mOOqJSCcNxWEnMyA"
-access_token_secret = "PWH8ZMAjATof27sJdthAVlH5qEDlbwP3I15xn7sDuDE3j"
+#Auth
+consumer_key = os.getenv("consumer_key")
+consumer_secret = os.getenv("consumer_secret")
+access_token = os.getenv("access_token")
+access_token_secret = os.getenv("access_token_secret")
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-# print("Username from extension:", str(sys.argv[1]))
+#print("Username from extension:", str(sys.argv[1]))
 tweets = api.user_timeline(screen_name=str(sys.argv[1]),
                            count=50,
                            include_rts=True,
@@ -25,16 +29,12 @@ tweets = api.user_timeline(screen_name=str(sys.argv[1]),
 oldest_id = tweets[-1].id
 all_tweets = []
 all_tweets.extend(tweets)
-print('N of tweets downloaded till now {}'.format(len(all_tweets)))
+#print('N of tweets downloaded till now {}'.format(len(all_tweets)))
 
-os.environ["EAI_USERNAME"] = 'nutheti.laasya@gmail.com'
-os.environ["EAI_PASSWORD"] = 'Rintern@1311'
+#ExpertNLP api
 client = ExpertAiClient()
-#texttweets = [[tweet.full_text] for idx, tweet in enumerate(all_tweets)]
-#text = [''.join(ele) for ele in texttweets]
-#text = " ".join(str(x) for x in text)
 
-
+#Clean text
 def removeEmoji(text):
     regrex_pattern = re.compile(pattern="["
                                 u"\U0001F600-\U0001F64F"  # emoticons
@@ -66,8 +66,9 @@ def removeSpecialChar(text):
     regrex_pattern = re.compile(
         pattern="[^0-9a-zA-Z ]+", flags=re.UNICODE)
     return regrex_pattern.sub(r'', text)
-
 #print(all_tweets[0])
+
+#Individual sentiment of tweets
 individual_sentiment = []
 def sentiment(tweets, all_tweets):
     for i in all_tweets:
@@ -86,40 +87,40 @@ def sentiment(tweets, all_tweets):
         individual_sentiment.append(document.sentiment.overall)
     return individual_sentiment
 rate_list = sentiment(tweets, all_tweets)
-print(mean(rate_list)
 
-#neg_count = len(list(filter(lambda x: (x < 0), rate_list)))
-#pos_count = len(list(filter(lambda x: (x > 0), rate_list)))
-#neut_count = len(list(filter(lambda x: (x == 0), rate_list)))
+#No. of neg, pos, neut tweets
+neg_count = len(list(filter(lambda x: (x < 0), rate_list)))
+pos_count = len(list(filter(lambda x: (x > 0), rate_list)))
+neut_count = len(list(filter(lambda x: (x == 0), rate_list)))
 
-#print("Positive numbers in the list: ", pos_count)
-#print("Negative numbers in the list: ", neg_count)
-#print("Neutral numbers in the list: ", neut_count)
+#Overall sentiment of the tweets
+texttweets = [[tweet.full_text] for idx, tweet in enumerate(all_tweets)]
+text = [''.join(ele) for ele in texttweets]
+text = " ".join(str(x) for x in text)
+text = removeEmoji(text)
+text = removeUsername(text)
+text = removeURL(text)
+text = removeLine(text)
+text = removeSpecialChar(text)
+#print(text.encode('utf-8'))
+#print(text)
+language = 'en'
+document = client.specific_resource_analysis(
+    body={"document": {"text": text}},
+    params={'language': language, 'resource': 'sentiment'})
 
-#def visualize_sentiment(rate_list):
-#    positive = 0
-#    negative = 0
-#    neutral = 0
-#    for i in individual_sentiment:
-#        if i < 0.0:
-#            negative = negative + 1
-#        elif i > 0.0:
-#            positive = positive + 1
-#       elif i == 0.0:
-#            neutral = neutral + 1
-#    return [positive, negative, neutral]
 
-#print(visualize_sentiment(sentiment(tweets, all_tweets)))
-
-#text = removeEmoji(text)
-#text = removeUsername(text)
-#text = removeURL(text)
-#text = removeLine(text)
-#text = removeSpecialChar(text)
-# print(text.encode('utf-8'))
-# print(text)
-#language = 'en'
-#document = client.specific_resource_analysis(
-#    body={"document": {"text": text}},
-#    params={'language': language, 'resource': 'sentiment'})
-#print("sentiment:", document.sentiment.overall)
+#User Details
+user = api.get_user(sys.argv[1])
+#User name
+print(user.name)
+#Account created at
+print(str(user.created_at))
+#Follower count
+print(str(user.followers_count))
+#Friends count (No. of people the user is following)
+print(str(user.friends_count))
+#Individual sentiment (positive, negative and neutral tweet count)
+print(pos_count, neg_count, neut_count)
+#Overall sentiment of the account
+print(document.sentiment.overall)
