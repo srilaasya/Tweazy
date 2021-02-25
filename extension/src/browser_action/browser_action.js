a8c3c056d84bda715e0400bd81dd0c60bbc62047
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-02-26 02:13:28 +0530

    ext: render output
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- extension/src/browser_action/browser_action.js
+++ extension/src/browser_action/browser_action.js
@@ -21,57 +21,64 @@ document.getElementById("getUsernames").addEventListener("click", () => {
 			code: "(" + getUsernames + ")();",
 		},
 		(usernames) => {
-
-
-			function addDataToExtension(userName,date,followers,following,pos,neg,neutral,overall){
-				var bodyDiv =document.getElementById("mainPopup")
-				while(bodyDiv.firstChild){
+			function addDataToExtension(
+				userName,
+				date,
+				followers,
+				following,
+				pos,
+				neg,
+				neutral,
+				overall
+			) {
+				var bodyDiv = document.getElementById("mainPopup");
+				while (bodyDiv.firstChild) {
 					bodyDiv.removeChild(bodyDiv.firstChild);
 				}
-				var Name=document.createElement("h4");
-				Name.innerText="User Name "+ userName;
-				var joined=document.createElement("h5");
-				joined.innerText="Joined on "+date;
-				var Followers=document.createElement("h5");
-				Followers.innerText="Followers "+followers+" "+"Following "+following;
+				var Name = document.createElement("h4");
+				Name.innerText = "User Name " + userName;
+				var joined = document.createElement("h5");
+				joined.innerText = "Joined on " + date;
+				var Followers = document.createElement("h5");
+				Followers.innerText =
+					"Followers " + followers + " " + "Following " + following;
 
-				var innerDiv=document.createElement('div');
+				var innerDiv = document.createElement("div");
 				innerDiv.appendChild(Name);
-				innerDiv.appendChild(document.createElement('br'));
+				innerDiv.appendChild(document.createElement("br"));
 				innerDiv.appendChild(joined);
-				innerDiv.appendChild(document.createElement('br'));
+				innerDiv.appendChild(document.createElement("br"));
 				innerDiv.appendChild(Followers);
 				bodyDiv.appendChild(innerDiv);
-				 
-				var outer=document.createElement("ul");
-				ul.classList.add("bar-graph");
 
-				var liForPositive=document.createElement('li');
+				var outer = document.createElement("ul");
+				outer.classList.add("bar-graph");
+
+				var liForPositive = document.createElement("li");
 				liForPositive.classList.add("bar", "success");
-				liForPositive.style.height=(pos/50)*100+"%";
-				var divPos=document.createElement('div');
-				divPos.innerText="positive - "+pos;
+				liForPositive.style.height = (pos / 50) * 100 + "%";
+				var divPos = document.createElement("div");
+				divPos.innerText = "positive - " + pos;
 				liForPositive.appendChild(divPos);
 
-				var liForNegative=document.createElement('li');
+				var liForNegative = document.createElement("li");
 				liForNegative.classList.add("bar", "alert");
-				liForNegative.style.height=(neg/50)*100+"%";
-				var divNeg=document.createElement('div');
-				divNeg.innerText="Negative - "+neg;
+				liForNegative.style.height = (neg / 50) * 100 + "%";
+				var divNeg = document.createElement("div");
+				divNeg.innerText = "Negative - " + neg;
 				liForNegative.appendChild(divNeg);
 
-				var liForNeutral=document.createElement('li');
+				var liForNeutral = document.createElement("li");
 				liForNeutral.classList.add("bar", "warning");
-				liForNeutral.style.height=(neutral/50)*100+"%";
-				var divNeu=document.createElement('div');
-				divNeu.innerText="Neutral - "+neutral;
+				liForNeutral.style.height = (neutral / 50) * 100 + "%";
+				var divNeu = document.createElement("div");
+				divNeu.innerText = "Neutral - " + neutral;
 				liForNeutral.appendChild(divNeu);
 
 				outer.appendChild(liForPositive);
 				outer.appendChild(liForNegative);
 				outer.appendChild(liForNeutral);
 				bodyDiv.appendChild(outer);
-
 			}
 			let check = 0;
 			document.getElementById(
@@ -97,50 +104,40 @@ document.getElementById("getUsernames").addEventListener("click", () => {
 				.querySelectorAll("button.usernameButton")
 				.forEach((ele) => {
 					ele.addEventListener("click", () => {
+						console.log("Sending", ele.innerText, "to NLP API");
 						fetch("http://localhost:8000/getRating", {
 							method: "post",
 							headers: {
 								"Content-type":
 									"application/x-www-form-urlencoded; charset=UTF-8",
 							},
-							body: "username=n_sri_laasya",
+							body: `username=${ele.innerText}`,
 						})
-							.then((response) => {
-								if (response.status === 200) {
-									return response.json();
+							.then((res) => {
+								if (res.status === 200) {
+									return res.json();
 								} else {
 									throw new Error(
-										"Something went wrong on api server!"
+										"Something went wrong on API server!"
 									);
 								}
 							})
-							.then((response) => {
-								console.debug(response);
-								// ...
+							.then((res) => {
+								console.log("API response is:", res);
+								addDataToExtension(
+									res.username,
+									res.joined,
+									res.followers,
+									res.following,
+									res.positive,
+									res.negative,
+									res.neutral,
+									res.overall
+								);
 							})
 							.catch((error) => {
 								console.error(error);
 							});
-						// fetch("/getRating", {
-						// 					method: "POST",
-						// 					body: '{"username": ele.innerText}'
-						// 				})
-						// 					.then(function(response) {
-						// 						if (response.status !== 200) {
-						// 							alert(
-						// 								"Looks like there was a problem. Status Code: " +
-						// 									response.status
-						// 							);
-						// 							return;
-						// 						}
-						// 						response.json().then(function(data) {
-						// 							console.log(data)
-						// 						});
-						// 					})
-						// 					.catch(function(err) {
-						// 						alert("Fetch Error :-S", err);
-						// 					});
-						// 			});
 					});
 				});
 		}
