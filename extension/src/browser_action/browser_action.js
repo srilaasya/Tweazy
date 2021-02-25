163cdb41b7b62d99bafa2ed3bd4cf72c3fef7812
Yashank18 <yashank7696@gmail.com>
2021-02-26 01:17:19 +0530

    Extension Bar Graph

--- extension/src/browser_action/browser_action.js
+++ extension/src/browser_action/browser_action.js
@@ -21,6 +21,58 @@ document.getElementById("getUsernames").addEventListener("click", () => {
 			code: "(" + getUsernames + ")();",
 		},
 		(usernames) => {
+
+
+			function addDataToExtension(userName,date,followers,following,pos,neg,neutral,overall){
+				var bodyDiv =document.getElementById("mainPopup")
+				while(bodyDiv.firstChild){
+					bodyDiv.removeChild(bodyDiv.firstChild);
+				}
+				var Name=document.createElement("h4");
+				Name.innerText="User Name "+ userName;
+				var joined=document.createElement("h5");
+				joined.innerText="Joined on "+date;
+				var Followers=document.createElement("h5");
+				Followers.innerText="Followers "+followers+" "+"Following "+following;
+
+				var innerDiv=document.createElement('div');
+				innerDiv.appendChild(Name);
+				innerDiv.appendChild(document.createElement('br'));
+				innerDiv.appendChild(joined);
+				innerDiv.appendChild(document.createElement('br'));
+				innerDiv.appendChild(Followers);
+				bodyDiv.appendChild(innerDiv);
+				 
+				var outer=document.createElement("ul");
+				ul.classList.add("bar-graph");
+
+				var liForPositive=document.createElement('li');
+				liForPositive.classList.add("bar", "success");
+				liForPositive.style.height=(pos/50)*100+"%";
+				var divPos=document.createElement('div');
+				divPos.innerText="positive - "+pos;
+				liForPositive.appendChild(divPos);
+
+				var liForNegative=document.createElement('li');
+				liForNegative.classList.add("bar", "alert");
+				liForNegative.style.height=(neg/50)*100+"%";
+				var divNeg=document.createElement('div');
+				divNeg.innerText="Negative - "+neg;
+				liForNegative.appendChild(divNeg);
+
+				var liForNeutral=document.createElement('li');
+				liForNeutral.classList.add("bar", "warning");
+				liForNeutral.style.height=(neutral/50)*100+"%";
+				var divNeu=document.createElement('div');
+				divNeu.innerText="Neutral - "+neutral;
+				liForNeutral.appendChild(divNeu);
+
+				outer.appendChild(liForPositive);
+				outer.appendChild(liForNegative);
+				outer.appendChild(liForNeutral);
+				bodyDiv.appendChild(outer);
+
+			}
 			let check = 0;
 			document.getElementById(
 				"tweazyHeading"
@@ -45,7 +97,7 @@ document.getElementById("getUsernames").addEventListener("click", () => {
 				.querySelectorAll("button.usernameButton")
 				.forEach((ele) => {
 					ele.addEventListener("click", () => {
-						fetch("https://tweazy.herokuapp.com/getRating", {
+						fetch("http://localhost:8000/getRating", {
 							method: "post",
 							headers: {
 								"Content-type":
