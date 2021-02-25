64de2a9e89238a4ca8e2b74509d0ab851b9f6986
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-02-25 18:38:18 +0530

    ext: Refactor JS
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- extension/src/browser_action/browser_action.js
+++ extension/src/browser_action/browser_action.js
@@ -1,25 +1,18 @@
 document.getElementById("getUsernames").addEventListener("click", () => {
-	console.log("Popup DOM fully loaded and parsed");
 	function getUsernames() {
 		let usernames = [];
 		document
 			.querySelectorAll(
 				"span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0"
 			)
-			.forEach((element) => {
-				if (element.innerText.match("^@[a-zA-Z0-9_]{0,15}$"))
-					if (
-						!usernames.includes(
-							element.innerText
-								.match("^@[a-zA-Z0-9_]{0,15}")[0]
-								.substring(1)
-						)
-					)
-						usernames.push(
-							element.innerText
-								.match("^@[a-zA-Z0-9_]{0,15}")[0]
-								.substring(1)
-						);
+			.forEach((ele) => {
+				let usernameRegExp = ele.innerText.match(
+					"^@[a-zA-Z0-9_]{0,15}$"
+				);
+				if (usernameRegExp) {
+					let username = usernameRegExp[0].substring(1);
+					usernames.includes(username) || usernames.push(username);
+				}
 			});
 		return usernames;
 	}
@@ -27,16 +20,16 @@ document.getElementById("getUsernames").addEventListener("click", () => {
 		{
 			code: "(" + getUsernames + ")();",
 		},
-		(results) => {
+		(usernames) => {
 			let check = 0;
 			document.getElementById("greetings").innerText = "Tweazy found:";
 			document.getElementById("getUsernames").hidden = true;
 			let p = document.createElement("p");
 			p.innerText = "Click any username to analyze";
 			document.getElementById("printUsernames").appendChild(p);
-			results[0].forEach((res) => {
-				let btn = document.createElement("BUTTON");
-				btn.innerHTML = results[0][check++];
+			usernames[0].forEach((res) => {
+				let btn = document.createElement("button");
+				btn.innerHTML = usernames[0][check++];
 				btn.style.marginBottom = "10px";
 				btn.classList.add("btn");
 				btn.classList.add("btn-outline-primary");
